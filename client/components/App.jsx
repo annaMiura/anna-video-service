4956d591c38ffe82dc78ad6660ad319e5f66f081
Anna Miura <annamaliamiura@gmail.com>
2019-03-08 14:41:30 -0800

    Fix axios calls

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -40,7 +40,7 @@ class App extends React.Component {
   }
   componentDidMount() {
     axios
-      .get('http://ec2-54-183-172-9.us-west-1.compute.amazonaws.com:3049/api/videos')
+      .get('/api/videos')
       .then(
         result => {
           this.setState({
@@ -57,7 +57,7 @@ class App extends React.Component {
         }
       )
       .then(() => {
-        axios.get('http://ec2-54-183-172-9.us-west-1.compute.amazonaws.com:3049/api/users').then(
+        axios.get('/api/users').then(
           result => {
             this.setState({
               isLoaded: true,
@@ -74,7 +74,7 @@ class App extends React.Component {
         );
       })
       .then(() => {
-        axios.get('http://ec2-54-183-172-9.us-west-1.compute.amazonaws.com:3049/api/games').then(
+        axios.get('/api/games').then(
           result => {
             this.setState({
               isLoaded: true,
